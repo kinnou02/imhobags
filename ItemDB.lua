f5c7305e07ed45505893fdf342d172e8cc436576
imhothar <none>
2012-02-25 15:12:16 +0100

    Category sorting and /reloadui fix
    * Added a "character"-stored table to saved variables to have data ready after a /reloadui
    * German locale for primary item categories required for locale-dependent sorting
    * ItemDB:GetItems() for selecting category-grouped and sorted items from the database

--- ItemDB.lua
+++ ItemDB.lua
@@ -1,19 +1,24 @@
 local _G = _G
+local ipairs = ipairs
 local pairs = pairs
 local print = print
 local setfenv = setfenv
+local string = string
 local table = table
 local tostring = tostring
 local type = type
 
 local dump = dump
+
 local Event = Event
 local Inspect = Inspect
 local Utility = Utility
 
 local playerItemMatrix = { items = { }, slots = { } }
-local itemMatrix = playerItemMatrix
+local itemMatrix
 local readonly = true
+
+local categoryCache = { } -- Used for avoiding string.match on known categories
 
 setfenv(1, ImhoBags)
 ItemDB = { }
@@ -23,24 +28,32 @@ ItemDB = { }
 
 local function mergeSlotUpdate(slots)
 	for slot, item in pairs(slots) do
-		if(type(item) == "string") then
+		if(item) then
 			local details = Inspect.Item.Detail(slot)
-			dump(details)
 			itemMatrix.slots[slot] = details.type
 			if(itemMatrix.items[details.type] == nil) then
 				itemMatrix.items[details.type] = { }
 			end
 			itemMatrix.items[details.type][slot] = (details.stack or 1)
-		else
-			dump(item)
 		end
 	end
 end
 
 local function startupEnd()
-	-- A /reloadui does not trigger all the Event.Item.Slot events
-	-- so we have to scan everything manually
-	mergeSlotUpdate(Inspect.Item.List(Utility.Item.Slot.All()))
+	-- A /reloadui does not trigger all the Event.Item.Slot events as on loggin or teleport.
+	-- That's why we need a separate "character"-stored table with
+	-- readily available data after a /reloadui
+	playerItemMatrix = _G.ImhoBagsPlayerItemMatrix or playerItemMatrix
+	itemMatrix = playerItemMatrix
+
+	-- Ensure at least the shard table exists
+	if(_G.ImhoBagsItemMatrix == nil) then
+		_G.ImhoBagsItemMatrix = { }
+	end
+	local shardName = Inspect.Shard().name
+	if(_G.ImhoBagsItemMatrix[shardName] == nil) then
+		_G.ImhoBagsItemMatrix[shardName] = { }
+	end
 end
 
 local function saveVariables(addonName)
@@ -48,23 +61,142 @@ local function saveVariables(addonName)
 		return
 	end
 	
-	local shardName = Inspect.Shard().name
-	local playerName = Inspect.Unit.Detail("player").name
-	
-	if(_G.ImhoBagsItems == nil) then
-		_G.ImhoBagsItems = { }
+	_G.ImhoBagsItemMatrix[Inspect.Shard().name][Inspect.Unit.Detail("player").name] = playerItemMatrix
+	_G.ImhoBagsPlayerItemMatrix = playerItemMatrix
+end
+
+local function extractUnsortedPlayerItems(prefix, condense)
+	local items = { }
+	for itemType, slots in pairs(playerItemMatrix.items) do
+		-- We have to treat full and partial stacks differently
+		-- otherwise the user would not be able to select partial stacks
+		local usedFullSlots = { }
+		local usedPartialSlots = { }
+		local detail = Inspect.Item.Detail(itemType)
+		local stackMax = detail.stackMax or 0 -- non-stackable items have stackMax = nil and must not be condensed
+		for slot, stack in pairs(slots) do
+			-- Do not include bag slots
+			if(string.find(slot, prefix, 1, true) and not string.find(slot, "bg.", 3, true)) then
+				if(condense and stack == stackMax) then
+					table.insert(usedFullSlots, slot)
+				else
+					table.insert(usedPartialSlots, { slot, stack })
+				end
+			end
+		end
+		if(#usedFullSlots > 0) then
+			table.insert(items, { type = detail, slots = usedFullSlots, stack = #usedFullSlots * stackMax })
+		end
+		for k, v in ipairs(usedPartialSlots) do
+			table.insert(items, { type = detail, slots = { v[1] }, stack = v[2] })
+		end
 	end
-	if(_G.ImhoBagsItems[shardName] == nil) then
-		_G.ImhoBagsItems[shardName] = { }
+	return items
+end
+
+local function extractUnsortedCharacterItems(matrix, prefix, condense)
+	for itemType, slots in pairs(matrix.items) do
+		-- Technically, the player is not able to manipulate items of
+		-- other characters, but we keep the stacking consistent to
+		-- keep the spatial location as visual clue
+		local usedFullSlots = { }
+		local usedPartialSlots = { }
+		local detail = Inspect.Item.Detail(itemType)
+		local stackMax = detail.stackMax or 0 -- non-stackable items have stackMax = nil and must not be condensed
+		for slot, stack in pairs(slots) do
+			if(string.find(slot, prefix, 1, true) and not string.find(slot, "bg.", 3, true)) then
+				if(condense and stack == stackMax) then
+					table.insert(usedFullSlots, stack)
+				else
+					table.insert(usedPartialSlots, stack)
+				end
+			end
+		end
+		if(#usedFullSlots > 0) then
+			table.insert(items, { type = detail, stack = #usedFullSlots * stackMax })
+		end
+		for k, v in ipairs(usedPartialSlots) do
+			table.insert(items, { type = detail, stack = v })
+		end
 	end
-	_G.ImhoBagsItems[shardName][playerName] = playerItemMatrix
 end
 
 -- Public methods
 -- ============================================================================
 
-function ItemDB:IsReadonly()
-	return readonly
+--[[
+character: Either the name of a secodary character or "player"
+location: "inventory", "bank"
+condense: true to condense max stacks together into one displayed item
+predicate: a function to call for sorting items (a, b) => boolean
+	both parameters are item type tables as returned by Inspect.Item.Detail()
+return: For the player, the return value has the following structure:
+[#] = {
+	name = "Name of category",
+	[#] = {
+		type = result of Inspect.Item.Detail(type),
+		slots = { array of item slots }
+		stack = #, -- displayed stack size
+	}
+}
+For all other characters the structure is as follows:
+[#] = {
+	name = "Name of category",
+	[#] = {
+		type = result of Inspect.Item.Detail(type),
+		stack = #, -- displayed stack size
+	}
+}
+]]
+function ItemDB:GetItems(character, location, condense, predicate)
+	-- Find out the slot prefix we are lookign for
+	local prefix;
+	if(location == "inventory") then
+		prefix = Utility.Item.Slot.Inventory();
+	elseif(location == "bank") then
+		prefix = Utility.Item.Slot.Bank();
+	else
+		return {}
+	end
+	-- Find the correct item matrix
+	local matrix;
+	if(character == "player") then
+		matrix = playerItemMatrix;
+	else
+		character = _G.ImhoBagsItemMatrix[Inspect.Shard().name][character] or { items = { }, slots = { } }
+	end
+	-- Dump all relevant items into a temporary table to be sorted later
+	local items;
+	if(character == "player") then
+		items = extractUnsortedPlayerItems(prefix, condense)
+	else
+		items = extractUnsortedCharaterItems(matrix, prefix, condense)
+	end
+	-- Sort according to provided predicate
+	table.sort(items, function(a, b) return predicate(a.type, b.type) end)
+	-- Build the sorted category table
+	local result = { }
+	local categoryMap = { }
+	for _, item in ipairs(items) do
+		-- Find the localized category name for sorting
+		local categoryName = categoryCache[item.type.category]
+		if(categoryName == nil) then
+			categoryName = string.match(item.type.category, "(%w+)")
+			categoryName = L.CategoryNames[categoryName]
+			categoryCache[item.type.category] = categoryName
+		end
+		-- Add the item to the correct category, preserving item sorting order
+		local category = categoryMap[categoryName];
+		if(category == nil) then
+			table.insert(result, { name = categoryName })
+			category = result[#result]
+			categoryMap[categoryName] = category
+		end
+		table.insert(category, item)
+	end
+	-- And finally sort by category name
+	table.sort(result, function(a, b) return a.name < b.name end)
+	return result
 end
 
 table.insert(Event.Addon.Startup.End, { startupEnd, AddonName, "ItemDB_startupEnd" })
---
+++ locales/de.lua
@@ -0,0 +1,24 @@
+﻿-- Make sure to save this file in UTF-8 encoding
+
+if(Inspect.System.Language() ~= "German" ) then
+	return
+end
+
+setfenv(1, ImhoBags)
+L =
+{
+	-- Category translation
+	-- [english] = translated
+	-- Although categories can have multiple words (e.g. "weapon sword onehand")
+	-- only the first word is used for grouping/sorting and needs to be translated.
+	-- Should match the names in the auction house.
+	CategoryNames = {
+		armor = "Rüstung",
+		consumable = "Verbrauchsgüter",
+		container = "Behälter",
+		crafting = "Handwerk",
+		misc = "Verschiedenes",
+		planar = "Ebenenobjekte",
+		weapon = "Waffen",
+	},
+}
