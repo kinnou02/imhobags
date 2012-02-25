9eaeb8bad39d1dd877903a0b468f5375033c63fd
imhothar <none>
2012-02-25 23:59:27 +0100

    Utility method from riftui.com to retrieve colors for quality levels

---
+++ Utils.lua
@@ -0,0 +1,17 @@
+local rarityColors = {
+	sellable =	{ 0.34375, 0.34375, 0.34375 },
+	common =	{    0.98,    0.98,    0.98 },
+	uncommon =	{     0.0,   0.797,     0.0 },
+	rare =		{   0.148,   0.496,   0.977 },
+	epic =		{   0.676,   0.281,    0.98 },
+	quest =		{     1.0,     1.0,     0.0 },
+	relic =		{     1.0,     0.5,     0.0 },
+}
+
+setfenv(1, ImhoBags)
+Utils = { }
+
+function Utils.RarityColor(rarity)
+	local col = rarityColors[rarity] or rarityColors.common
+	return { r = col[1], g = col[2], b = col[3] }
+end
