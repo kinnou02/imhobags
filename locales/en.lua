ce23b1f1a326c6a7bbac36503fcab1eb7385377d
imhothar <none>
2012-02-26 01:17:12 +0100

    English locale file

---
+++ locales/en.lua
@@ -0,0 +1,24 @@
+﻿-- Make sure to save this file in UTF-8 encoding
+
+if(Inspect.System.Language() ~= "English" ) then
+	return
+end
+
+setfenv(1, ImhoBags)
+L =
+{
+	-- Category translation
+	-- [english] = translated
+	-- Although categories can have multiple words (e.g. "weapon sword onehand")
+	-- only the first word is used for grouping/sorting and needs to be translated.
+	-- Should match the names in the auction house.
+	CategoryNames = {
+		armor = "Armor",
+		consumable = "Consumables",
+		container = "Containers",
+		crafting = "Crafting",
+		misc = "Miscellaneous",
+		planar = "Planar",
+		weapon = "Weapons",
+	},
+}
