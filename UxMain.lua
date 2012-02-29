2acd729c50d0c866ce7a0fbc4eadd9f2e98b9836
imhothar <none>
2012-02-29 22:21:35 +0100

    ItemWindow positions are now persisted per account

--- UxMain.lua
+++ UxMain.lua
@@ -5,11 +5,13 @@ local _G = _G
 local pairs = pairs
 local table = table
 
+local UIParent = UIParent
+
 local Event = Event
 local UI = UI
 
 local defaultItemWindows =  {
---	BackpackItemWindow = { "inventory", UI.Native.BagInventory1 },
+	BackpackItemWindow = { "inventory", UI.Native.BagInventory1 },
 	BankItemWindow = { "bank", UI.Native.Bank },
 }
 
@@ -25,10 +27,34 @@ local function Ux_addonStartupEnd()
 	for k, v in pairs(defaultItemWindows) do
 		local title = L.WindowTitles[v[1]]
 		local window = Ux.ItemWindow.New(title,"player", v[1], true, v[2])
-		-- TODO: Restore window position
---		window:SetPoint("TOPLEFT", _G.UIParent, "TOPLEFT", 0, 0)
+
+		local position = _G.ImhoBagsWindowPositions[k]
+		if(position) then
+			window:SetPoint("TOPLEFT", UIParent, "TOPLEFT", position.x, position.y)
+		else
+			local width, height = UIParent:GetWidth(), UIParent:GetHeight()
+			window:SetPoint("TOPLEFT", UIParent, "TOPLEFT", (width - window:GetWidth()) / 2, (height - window:GetHeight()) / 2)
+		end
 		Ux[k] = window
 	end
 end
 
+local function Ux_savedVariablesLoadEnd(addonIdentifier)
+	if(addonIdentifier ~= identifier) then
+		return
+	end
+	_G.ImhoBagsWindowPositions = _G.ImhoBagsWindowPositions or { }
+end
+
+local function Ux_savedVariablesSaveBegin(addonIdentifier)
+	if(addonIdentifier ~= identifier) then
+		return
+	end
+	for k, v in pairs(defaultItemWindows) do
+		_G.ImhoBagsWindowPositions[k] = { x = Ux[k]:GetLeft(), y = Ux[k]:GetTop() }
+	end
+end
+
 table.insert(Event.Addon.Startup.End, { Ux_addonStartupEnd, identifier, "Ux_addonStartupEnd" })
+table.insert(Event.Addon.SavedVariables.Load.End, { Ux_savedVariablesLoadEnd, identifier, "Ux_savedVariablesLoadEnd" })
+table.insert(Event.Addon.SavedVariables.Save.Begin, { Ux_savedVariablesSaveBegin, identifier, "Ux_savedVariablesSaveBegin" })
