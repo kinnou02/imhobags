9a038d8dbb9b1f6f339649e865949d47aabbc257
imhothar <none>
2012-02-29 02:38:08 +0100

    Item windows are now created after addon startup completed

--- UxMain.lua
+++ UxMain.lua
@@ -2,15 +2,36 @@
 local addon = (...).data
 
 local _G = _G
+local pairs = pairs
+local table = table
 
+local Event = Event
 local UI = UI
+
+local defaultItemWindows =  {
+--	BackpackItemWindow = { "inventory", UI.Native.BagInventory1 },
+	BankItemWindow = { "bank", UI.Native.Bank },
+}
 
 setfenv(1, addon)
 Ux = Ux or { }
 
 Ux.Context = UI.CreateContext(identifier)
 
-local window = CreateItemWindow("Rucksack")
+--Private methods
+-- ============================================================================
+
+local function Ux_addonStartupEnd()
+	for k, v in pairs(defaultItemWindows) do
+		local title = L.WindowTitles[v[1]]
+		local window = Ux.ItemWindow.New(title,"player", v[1], true, v[2])
+		-- TODO: Restore window position
+		window:SetPoint("CENTER", _G.UIParent, "CENTER", 0, 0)
+		Ux[k] = window
+	end
+end
+--[[
+local window = Ux.ItemWindow.New("Rucksack","player", "bank", true)
 window:SetPoint("CENTER", _G.UIParent, "CENTER", 0, 0)
 Ux.BackpackWindow = window
 
@@ -18,6 +39,7 @@ local button = UI.CreateFrame("RiftButton", "", Ux.BackpackWindow)
 button:SetText("Test")
 button:SetPoint("BOTTOMCENTER", Ux.BackpackWindow, "BOTTOMCENTER", 0, -5)
 function button.Event:LeftClick()
-	local matrix = ItemDB.GetItemMatrix("player", "bank")
-	Ux.BackpackWindow:SetItemMatrix(matrix)
+	Ux.BackpackWindow:SetCharacter("player", "bank")
 end
+]]
+table.insert(Event.Addon.Startup.End, { Ux_addonStartupEnd, identifier, "Ux_addonStartupEnd" })
