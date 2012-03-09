3ba4a8b702b0678582951264d4e4c741dfe7acee
imhothar <none>
2012-03-09 02:54:25 +0100

    Added convenience variables to addon table

--- Main.lua
+++ Main.lua
@@ -4,6 +4,7 @@ if(Addon.toc.debug) then
 	_G[Addon.identifier] = private
 end
 
+local pairs = pairs
 local print = print
 local select = select
 local table = table
@@ -28,3 +29,25 @@ if(Addon.toc.debug) then
 else
 	log = function() end
 end
+
+-- Always available
+PlayerName = ""
+PlayerFaction = ""
+PlayerShard = Inspect.Shard().name
+
+local unitAvailableIndex
+local function unitAvailable(units)
+	for k, v in pairs(units) do
+		if(v == "player") then
+			local player = Inspect.Unit.Detail("player")
+			PlayerName = player.name
+			PlayerFaction = player.faction
+			
+			Event.Unit.Available[unitAvailableIndex][1] = function() end
+			break
+		end
+	end
+end
+
+table.insert(Event.Unit.Available, { unitAvailable, Addon.identifier, "unitAvailable" })
+unitAvailableIndex = #Event.Unit.Available
