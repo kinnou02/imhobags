072a3bc53bbb4f44d429e26e2a97a9b5830c37b7
imhothar <none>
2012-03-10 18:04:01 +0100

    Allow mail matrix to be queryed for items

--- MailMatrix.lua
+++ MailMatrix.lua
@@ -5,8 +5,9 @@ local ipairs = ipairs
 local next = next
 local pairs = pairs
 local pcall = pcall
-local table = table
 local setmetatable = setmetatable
+local string = string
+local table = table
 
 -- Globals
 local dump = dump
@@ -33,6 +34,41 @@ local function purge(matrix, mail, attachments)
 	end
 end
 
+local function extractUnsortedCharacterItems(matrix)
+	local items = { }
+	local success = true
+	for itemType, slots in pairs(matrix.items) do
+		-- Because the mail window does not allow for item manipulation
+		-- all stacks are condensed.
+
+		-- If looking at other characters item information might not be available.
+		-- In this case Inspect.Item.Detail throws an error and we need to remember
+		-- to ask later.
+		local result, detail = pcall(Inspect.Item.Detail, itemType)
+		success = success and result
+		if(result) then
+			for mail, count in pairs(slots) do
+				table.insert(items, { type = detail, slots = 1, stack = count })
+				items[#items].type.ImhoBags_mailSubject = matrix.mails[mail].subject
+			end
+		end
+	end
+	-- Get coin info
+	-- Won't work right now as coin attachments don't get tooltips
+	for mail, data in pairs(matrix.mails) do
+		if(data.coin) then
+			local type = {
+				ImhoBags_mailSubject = data.subject,
+				name = Utils.FormatCoin(data.coin),
+				icon = (data.coin > 10000 and [[Data/\UI\item_icons\loot_platinum_coins.dds]]) or
+					(data.coin > 100 and [[Data/\UI\item_icons\loot_gold_coins.dds]]) or [[Data/\UI\item_icons\loot_silver_coins.dds]],
+			}
+			table.insert(items, { type = type, slots = 1, stack = data.coin })
+		end
+	end
+	return items, 0, success
+end
+
 -- Public methods
 -- ============================================================================
 
@@ -44,8 +80,10 @@ function MailMatrix.New()
 			}
 ]]		},
 		mails = {
---			[mail] = { types }
-		},
+--[[		[mail] = {
+				[#] = type,
+				subject = "sender: subject"
+]]		},
 		lastUpdate = -1, -- Forced to -1 on save
 	}
 	return setmetatable(matrix, MailMatrix_matrixMetaTable)
@@ -57,10 +95,13 @@ Also available as instance metamethod.
 ]]
 function MailMatrix.MergeMail(matrix, mail)
 	local attachments = { }
+	local coin = nil
 	for _, item in ipairs(mail.attachments) do
 		item = Inspect.Item.Detail(item)
 		if(item.type) then -- Is nil for money
 			attachments[item.type] = (attachments[item.type] or 0) + (item.stack or 1)
+		else
+			coin = item.coin
 		end
 	end
 	
@@ -68,10 +109,15 @@ function MailMatrix.MergeMail(matrix, mail)
 	if(next(attachments) == nil) then
 		log("deleting mail")
 		matrix.mails[mail.id] = nil
+		matrix.subjects[mail.id] = nil
 		return
 	end
 	
-	local t = { }
+	local t = matrix.mails[mail.id] or {
+		subject = string.format("%s: %s", mail.from, mail.subject),
+		body = mail.body,
+		coin = coin,
+	}
 	for type in pairs(attachments) do
 		table.insert(t, type)
 	end
@@ -87,17 +133,30 @@ function MailMatrix.MergeMail(matrix, mail)
 	log("update", "mail", mail.id, matrix.lastUpdate)
 end
 
--- Remove mails from the matrix which no longer exist
-function MailMatrix.Purge(matrix, mails)
-	local empty = { }
-	for mail in pairs(matrix.mails) do
-		if(mails[mail] == nil) then
-			log("purge", "mail", mail, matrix.lastUpdate)
-			purge(matrix, mail, empty)
-			matrix.mails[mail] = nil
-		end
-	end
-	matrix.lastUpdate = Inspect.Time.Real() -- Inspect.Time.Frame() is not good enough and can cause multiple updates per frame
+--[[
+Get the list of items for this container in one flat table and no particular sorting.
+The returned list serves as staging base for further operations.
+Also available as instance metamethod.
+condensed: True to condense max stacks together into one displayed item (ignored for mails)
+return: items, empty, success
+	"success" determines whether all items could be retrieved successfully or
+	whether the local item cache is incomplete and you have to try again later.
+	This is common if requesting items from other characters than the player and
+	you need to call the function later to retrieve the remaining items until succes becomes true.
+	
+	The other return values are as follows:
+	
+	items = { -- Array
+		[#] = {
+			type = result of Inspect.Item.Detail(type),-- Includes ImhoBags_mailSubject field for mail subject line,
+			slots = number, -- always 1 for mails
+			stack = #, -- displayed stack size
+		}
+	}
+	empty = number
+]]
+function MailMatrix.GetUnsortedItems(matrix, condensed)
+	return extractUnsortedCharacterItems(matrix)
 end
 
 -- Get the amount of items in this matrix of the given item type (including bags).
@@ -123,8 +182,8 @@ local MailMatrix_matrixMetaTable = {
 	__index = {
 		GetAllItemTypes = MailMatrix.GetAllItemTypes,
 		GetItemCount = MailMatrix.GetItemCount,
+		GetUnsortedItems = MailMatrix.GetUnsortedItems,
 		MergeMail = MailMatrix.MergeMail,
-		Purge = MailMatrix.Purge,
 	}
 }
 
