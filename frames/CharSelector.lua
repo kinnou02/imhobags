453f4d00d9e6cc40bbdb4a774aa13eb6d3bc8605
imhothar <none>
2012-03-17 03:05:18 +0100

    CharSelector now tracks changes to showEnemyFaction config

--- frames/CharSelector.lua
+++ frames/CharSelector.lua
@@ -18,18 +18,78 @@ local borderWidth = 2
 -- Private methods
 -- ============================================================================
 
+local function showMenu(self)
+	self.menu:SetVisible(true)
+	self.menu:SetWidth(math.max(self:GetWidth(), self.menu.contentWidth))
+end
+
+local function getButton(self, i)
+	if(self.menu.buttons[i]) then
+		return self.menu.buttons[i]
+	else
+		local btn = UI.CreateFrame("Frame", "", self.menu)
+		local label = UI.CreateFrame("Text", "", btn)
+		label:SetFontSize(14)
+		label:SetPoint("CENTER", btn, "CENTER")
+		label:SetText("X")
+		btn.label = label
+		
+		if(i == 1) then
+			btn:SetPoint("TOPLEFT", self.menu.background, "TOPLEFT")
+			btn:SetPoint("TOPRIGHT", self.menu.background, "TOPRIGHT")
+		else
+			btn:SetPoint("TOPLEFT", self.menu.buttons[i - 1], "BOTTOMLEFT")
+			btn:SetPoint("TOPRIGHT", self.menu.buttons[i - 1], "BOTTOMRIGHT")
+		end
+
+		btn:SetLayer(self.menu.background:GetLayer() + 1)
+		
+		function btn.Event:MouseIn()
+			btn:SetBackgroundColor(1, 1, 1, 0.3)
+		end
+		function btn.Event:MouseOut()
+			btn:SetBackgroundColor(1, 1, 1, 0)
+		end
+		function btn.Event.LeftDown()
+			btn:SetBackgroundColor(1, 1, 1, 0)
+			self.menu:SetVisible(false)
+			self:SetText(label:GetText())
+			self.callback(label:GetText())
+		end
+		table.insert(self.menu.buttons, btn)
+		return btn
+	end
+end
+
+local function updateMenu(self)
+	local chars = ItemDB.GetAvailableCharacters()
+	
+	local width, height = 0, 0
+	for i = 1, #chars do
+		local btn = getButton(self, i)
+		btn.label:SetText(chars[i])
+		btn:SetVisible(true)
+		btn:SetHeight(btn.label:GetFullHeight())
+		
+		height = height + btn:GetHeight()
+		width = width + btn.label:GetFullWidth()
+	end
+	for i = #chars + 1, #self.menu.buttons do
+		self.menu.buttons[i]:SetVisible(false)
+	end
+	self.menu:SetWidth(math.max(width, self:GetWidth()))
+	self.menu:SetHeight(height + 2 * borderWidth)
+end
+
 -- Public methods
 -- ============================================================================
 
-function Ux.CharSelector.New(parent, characters, current, callback)
+function Ux.CharSelector.New(parent, current, callback)
 	local frame = UI.CreateFrame("RiftButton", "", parent)
-	frame.characters = characters
 	frame:SetText(current)
+	frame.callback = callback
 	
-	function frame.Event:LeftPress()
-		self.menu:SetVisible(true)
-		self.menu:SetWidth(math.max(self:GetWidth(), self.menu.contentWidth))
-	end
+	frame.Event.LeftPress = showMenu
 	
 	local menu = UI.CreateFrame("Frame", "", parent)
 	menu:SetLayer(100)
@@ -43,41 +103,15 @@ function Ux.CharSelector.New(parent, characters, current, callback)
 	menu.background:SetPoint("BOTTOMRIGHT", menu, "BOTTOMRIGHT", -borderWidth, -borderWidth)
 	menu.background:SetBackgroundColor(0, 0, 0)
 	
-	local prevAnchor = menu.background
-	local prevPoint = "TOP"
-	local height = 0
-	for _, char in pairs(characters) do
-		local back = UI.CreateFrame("Frame", "", menu)
-		
-		local label = UI.CreateFrame("Text", "", back)
-		label:SetFontSize(14)
-		label:SetPoint("CENTER", back, "CENTER", 0, 0)
-		label:SetText(char)
-
-		back:SetPoint("TOPLEFT", prevAnchor, prevPoint .. "LEFT")
-		back:SetPoint("TOPRIGHT", prevAnchor, prevPoint .. "RIGHT")
-		back:SetHeight(label:GetHeight())
-		back:SetLayer(menu.background:GetLayer() + 1)
-		prevPoint = "BOTTOM"
-		prevAnchor = back
-		
-		menu.contentWidth = math.max(menu.contentWidth, label:GetFullWidth())
-		height = height + label:GetHeight()
-		
-		function back.Event:MouseIn()
-			back:SetBackgroundColor(1, 1, 1, 0.3)
+	menu.buttons = { }
+	
+	updateMenu(frame)
+	
+	table.insert(ImhoEvent.Config, { function(name)
+		if(name == "showEnemyFaction") then
+			updateMenu(frame)
 		end
-		function back.Event:MouseOut()
-			back:SetBackgroundColor(1, 1, 1, 0)
-		end
-		function back.Event:LeftDown()
-			back:SetBackgroundColor(1, 1, 1, 0)
-			menu:SetVisible(false)
-			frame:SetText(label:GetText())
-			callback(label:GetText())
-		end
-	end
-	menu:SetHeight(height + 2 * borderWidth)
+	end, Addon.identifier, "Ux.CharSelector_updateMenu" })
 	
 	return frame
 end
