b4d094e6da2631ecaf87d24d0f6ff31618c7d1e3
imhothar <none>
2012-03-24 00:01:36 +0100

    Added enUS localization for autoOpen config window option

--- locales/enUS.lua
+++ locales/enUS.lua
@@ -125,6 +125,7 @@ Available config options:
 			condensed = "Choose whether multiple full stacks of the same item are condensed into one button or whether each stack is displayed separately.",
 			packGroups = "Choose whether multiple small groups should be packed into one line where possible instead of creating a new line for every single group. The former may result in inconsistent group sorting but can drastically decrease the size of the item windows.",
 			itemButtonSkin = "Choose how item buttons should be rendered. The left option requires more resources than the right one but looks better. If you play with a very low UI scale the right button skin might look better. Note that changes to this option require a /reloadui before becoming effective.",
+			autoOpen = "Open and close the ImhoBags inventory or bank windows automatically when the respective Trion windows are opened and closed.",
 		},
 	},
 }
