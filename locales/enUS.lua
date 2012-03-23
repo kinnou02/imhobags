8cf5a02b4aeacd54beac2d9c59147a5658e2de2e
imhothar <none>
2012-03-23 17:45:55 +0100

    Additions to enUS locale for config window

--- locales/enUS.lua
+++ locales/enUS.lua
@@ -119,6 +119,8 @@ Available config options:
 		-- All text for the configuration window goes here
 		ConfigWindow = {
 			title = "Imhothar's Bags Configuration",
+			showTooltips = "Show Slash Commands",
+			appearanceSection = "Appearance",
 			condensed = "Choose whether multiple full stacks of the same item are condensed into one button or whether each stack is displayed separately.",
 			packGroups = "Choose whether multiple small groups should be packed into one line where possible instead of creating a new line for every single group. The former may result in inconsistent group sorting but can drastically decrease the size of the item windows.",
 			itemButtonSkin = "Choose how item buttons should be rendered. The left option requires more resources than the right one but looks better. If you play with a very low UI scale the right button skin might look better. Note that changes to this option require a /reloadui before becoming effective.",
