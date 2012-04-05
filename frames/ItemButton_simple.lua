2bb00220554d4020b900b6db53e7ada64f5b9ad9
imhothar <none>
2012-04-06 00:37:41 +0200

    Show bound symbol in topleft to avoid layout issues on small buttons

--- frames/ItemButton_simple.lua
+++ frames/ItemButton_simple.lua
@@ -115,7 +115,7 @@ function Ux.ItemButton_simple.New(parent)
 	self.slotsText:SetFontColor(0.8, 0.8, 0.8)
 	
 	self.bound = UICreateFrame("Texture", "", self)
-	self.bound:SetPoint("TOPRIGHT", self.icon, "TOPRIGHT")
+	self.bound:SetPoint("TOPLEFT", self.icon, "TOPLEFT")
 	self.bound:SetTexture("Rift", [[Data/\UI\ability_icons\soulbind.dds]])
 	self.bound:SetWidth(self.icon:GetWidth() / 3)
 	self.bound:SetHeight(self.bound:GetWidth())
