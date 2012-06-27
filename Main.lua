6f933980feb03f88ea3c9fba1f476b243a1e3300
imhothar <none>
2012-06-27 16:50:28 +0200

    Adjusted logon translator advertisement once again

--- Main.lua
+++ Main.lua
@@ -17,8 +17,7 @@ local lang = Inspect.System.Language()
 if(lang == "Korean" or lang == "French" or lang == "Russian") then
 	Command.Console.Display("general", true, "<font color='#FFFF00'>ImhoBags is looking for " .. lang .. " translators and reviewers!\nCheck by the official Rift forums in the 'Addon API Development' section if you'd like to help!</font>", true)
 end
-Command.Console.Display("general", false, "Try out the new popup menu with <font color='#00FF00'>/imhobags menu</font>", true)
-print("Works great as a macro button for accessing all your inventories without clumsy slash commands.")
+Command.Console.Display("general", false, "Try out the new popup menu with <font color='#00FF00'>/imhobags menu</font> (try putting it in a macro!)", true)
 
 setfenv(1, private)
 
