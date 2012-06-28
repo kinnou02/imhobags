a5d2357c95a6eb0935d3db8b470fba63fc41cc8d
imhothar <none>
2012-06-28 21:42:25 +0200

    Removed test cases

--- PublicInterface.lua
+++ PublicInterface.lua
@@ -20,8 +20,3 @@ private.Trigger.Item.Standard.Right, ImhoBags.Event.Item.Standard.Right = Utilit
 private.Trigger.Item.Standard.Left, ImhoBags.Event.Item.Standard.Left = Utility.Event.Create("ImhoBags", "Event.Item.Standard.Left")
 private.Trigger.Item.Standard.Drag, ImhoBags.Event.Item.Standard.Drag = Utility.Event.Create("ImhoBags", "Event.Item.Standard.Drag")
 private.Trigger.Item.Standard.Drop, ImhoBags.Event.Item.Standard.Drop = Utility.Event.Create("ImhoBags", "Event.Item.Standard.Drop")
-
-table.insert(ImhoBags.Event.Item.Standard.Right, { function(params) print("Right", params.id) end, "ImhoBags", "" })
-table.insert(ImhoBags.Event.Item.Standard.Left, { function(params) print("Left", params.id) end, "ImhoBags", "" })
-table.insert(ImhoBags.Event.Item.Standard.Drag, { function(params) print("Drag", params.id) end, "ImhoBags", "" })
-table.insert(ImhoBags.Event.Item.Standard.Drop, { function(params) print("Drop", params.id) end, "ImhoBags", "" })
