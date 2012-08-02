d436c8a14104a147dfc1079a08524dce64198aa8
imhothar <none>
2012-08-02 17:14:57 +0200

    Added code to find out details about the chosen code path in error reports

--- ItemHandler.lua
+++ ItemHandler.lua
@@ -1,10 +1,12 @@
 local Addon, private = ...
 
 -- Builtins
+local _G = _G
 local pcall = pcall
 
 -- Globals
 local Command = Command
+local ImhoBags = ImhoBags
 
 setfenv(1, private)
 ItemHandler = {
@@ -17,11 +19,30 @@ function ItemHandler.Standard.Right(id)
 		id = id,
 		cancel = false,
 	}
-	Trigger.Item.Standard.Right(params)
-	if(not params.cancel) then
-		pcall(Command.Item.Standard.Right, id)
+	-- temporary measure to find out which code path returns in the error reports
+	local BananAH = false
+	for k, v in _G.ipairs(_G.ImhoBags.Event.Item.Standard.Right) do
+		if(v[2] == "BananAH") then
+			BananAH = true
+		end
 	end
-	return params.cancel
+	if(BananAH) then
+		Trigger.Item.Standard.Right(params)
+		if(not params.cancel) then
+			pcall(Command.Item.Standard.Right, id)
+			return params.cancel
+		else
+			return params.cancel
+		end
+	else
+		Trigger.Item.Standard.Right(params)
+		if(not params.cancel) then
+			pcall(Command.Item.Standard.Right, id)
+			return params.cancel
+		else
+			return params.cancel
+		end
+	end
 end
 
 function ItemHandler.Standard.Left(id)
