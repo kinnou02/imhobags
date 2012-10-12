8cab130c04c74a2095d67ba02e67ad6b06fbf428
imhothar <none>
2012-10-12 11:18:41 +0200

    Fix title bar button texture size after async load

--- windows/ItemWindowTemplate/TitleBarButton.lua
+++ windows/ItemWindowTemplate/TitleBarButton.lua
@@ -24,10 +24,11 @@ function Ux.ItemWindowTemplate.TitleBarButton(parent, source, texture, dx, dy, o
 	self.Event.LeftClick = callback
 	
 	local tex = UICreateFrame("Texture", "", self)
-	tex:SetTextureAsync(source, texture)
 	tex:SetPoint("CENTER", self, "CENTER", offsetx or 0, offsety or 0)
-	tex:SetWidth(dx or tex:GetWidth())
-	tex:SetHeight(dy or tex:GetHeight())
+	tex:SetTextureAsync(source, texture, function()
+		tex:SetWidth(dx or tex:GetWidth())
+		tex:SetHeight(dy or tex:GetHeight())
+	end)
 
 	function self:SetTexture(source, texture, dx, dy, offsetx, offsety)
 		tex:SetTextureAsync(source, texture)
