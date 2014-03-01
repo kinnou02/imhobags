e6798afa71e18a5f3cb36d3ef079aa71527849ed
Amadeus <amadeus@myself.com>
2014-03-01 01:16:59 -0800

    Further refinements on sorting by equip slot

--- Sort.Default.lua
+++ Sort.Default.lua
@@ -14,9 +14,51 @@ local rarityOrder = {
 	empty = 0,
 }
 
+
 setfenv(1, private)
 Sort = Sort or { }
 Sort.Default = { }
+local equipSlotCache = { }
+
+-- Private methods
+-- ============================================================================
+local function getEquipSlot(item)
+	-- This function returns the slot that items are equipped on the player.  For example, a chest armor piece will return "chest".
+	-- It is important to note that, as of the writing of this function, only the feet, head, chest, shoulders, hands, legs, and
+	-- waist slots use different categories based upon the material of the item.  Therefore, those are the only ones that 
+	-- require special treatment.
+	--
+	-- The equipSlotCache table is used for efficiency.
+
+	local category = item.category or L.CategoryName.misc
+	local equipSlot = equipSlotCache[category]
+	if(equipSlot == nil) then
+		if (string.find(category,"feet")) then
+			equipSlot = "feet"
+		elseif (string.find(category,"head")) then
+			equipSlot = "head"			
+		elseif (string.find(category,"chest")) then
+			equipSlot = "chest"		
+		elseif (string.find(category,"shoulders")) then
+			equipSlot = "shoulders"		
+		elseif (string.find(category,"hands")) then
+			equipSlot = "hands"		
+		elseif (string.find(category,"waist")) then
+			equipSlot = "waist"							
+		elseif (string.find(category,"legs")) then
+			equipSlot = "legs"													
+		end
+		if(not equipSlot) then
+			equipSlot = category
+		end
+		--print("DEBUG: ", category, " == ", equipSlot)
+		equipSlotCache[category] = equipSlot
+	end
+	
+	return equipSlot
+end
+
+
 
 -- Public methods
 -- ============================================================================
@@ -42,13 +84,14 @@ function Sort.Default.ByItemRarity(type1, type2)
 	
 	----
 	-- If item is armor or weapons, and the item is rare or better, sort by armor/weapon category (equip. slot position) first.
-	if (rarityOrder[r1] >= 4 and rarityOrder[r2] >= 4) then
+	if (rarityOrder[r1] > 3 and rarityOrder[r2] > 3) then
 		local Category1 = Group.Default.GetLocalizedShortCategory(type1)
 		local Category2 = Group.Default.GetLocalizedShortCategory(type2)
-	
 		if (Category1 == "Armor" and Category2 == "Armor" or Category1 == "Weapons" and Category2 == "Weapons") then
-			if (type1.category ~= type2.category) then
-				return type1.category > type2.category
+			local equipSlot1 = getEquipSlot(type1)
+			local equipSlot2 = getEquipSlot(type2)
+			if (equipSlot1 ~= equipSlot2) then
+				return equipSlot1 > equipSlot2
 			end
 		end
 	end
